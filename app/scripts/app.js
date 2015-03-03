533ee7777e77811eec836be7933c67b37e5750d5
Bernardo Martins <bsamartins@gmail.com>
2015-03-03 15:50:34 +0000

    added front end

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mangaReaderMobileApp
+ * @description
+ * # mangaReaderMobileApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mangaReaderMobileApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mangaReaderMobileApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mangaReaderMobileApp
+ */
+angular.module('mangaReaderMobileApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
